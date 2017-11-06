4c7cdf39364a972b2018c39e71dc7158cc8dbaf9
Christopher Paul Namyst <cpnamyst79@gmail.com>
2017-11-06 07:44:20 -0500

    eggs and toast

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-$(function() {
+
 
   // get the json file
   $.get("https://www.reddit.com/r/aww/.json", function(data) {
-    var redditPosts = data.data.children;
-    redditPosts.forEach(function(blogPost) {
-      var postEl = $("<section class='post'>");
-      var titleEl = $("<h3>");
 
-    
+    var redditPosts = data.data.children;//stores info
+
+    for( var i = 1; i <= 10; i ++){ //loop to run thru posts, skipping the first one
+      var article = redditPosts[i];
+
+
+      var blogTitle = post.data.title;
+      var blogUrl = post.data.url;
+      var blogScore = post.data.score;
+      var blogAuthor = post.data.author;
+      var blogThumbnail = post.data.thumbnail;
+      var blogEl = $("<section>");
+      var blogPage = $("<div id='blogPage'>");
+
+
+
+      blogEl.append(blogContainer);
+      blogEl.append(blogTitle);
+      blogEl.append(blogUrl);
+      blogEl.append(blogScore);
+      blogEl.append(blogAuthor);
+      blogEl.appand(blogThumbnail);
+
+      
+
+
+
+
+
+
 
       $("#main").append(postEl);
 
-    });
 
-  });
-});
+}
